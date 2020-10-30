07063d0b29c618218538547784a7bb243296a88f
Fumikazu Fujiwara <github@ze.gs>
2020-10-30 22:39:45 +0900

    bugfix

--- kilo.js
+++ kilo.js
@@ -1,5 +1,6 @@
 #!/bin/env node
 const readline = require('readline');
+const pkg = require('./package');
 
 readline.emitKeypressEvents(process.stdin);
 function enableRawMode(){
@@ -7,21 +8,44 @@ function enableRawMode(){
 }
 
 function editorRefreshScreen(){
-  process.stdout.write("\x1b[2J", 4);
+  process.stdout.write("\x1b[?25l", 6);
   process.stdout.write("\x1b[H", 3);
   editorDrawRows();
   process.stdout.write("\x1b[H", 3);
+  process.stdout.write("\x1b[?25l", 6);
 }
 
 function editorReadKey(str, key) {
   if (key.ctrl && key.name === 'q') {
+    process.stdout.write("\x1b[2J", 4);
+    process.stdout.write("\x1b[H", 3);
     process.exit();
   }
 }
 
 function editorDrawRows() {
-  for (let y = 0; y < process.stdout.rows - 1 ; y++) {
-    process.stdout.write("~\r\n", 3);
+  for (let y = 0; y < process.stdout.rows ; y++) {
+    if (y == parseInt(process.stdout.rows / 3)) {
+      const welcome = `Kilo editor -- version ${pkg.version}`;
+      let welcomlen = welcome.length;
+      if (welcomlen > process.stdout.columns) welcomlen = process.stdout.columns;
+      let padding = parseInt((process.stdout.columns - welcomlen) / 2);
+      if (padding > 0) {
+        process.stdout.write("~", 1);
+        padding--;
+      }
+      while (padding > 0){
+        process.stdout.write(" ", 1);
+        padding--;
+      }
+      process.stdout.write(welcome,welcomlen);
+    } else {
+      process.stdout.write("~", 1);
+    }
+    process.stdout.write("\x1b[K", 3);
+    if (y < process.stdout.rows - 1) {
+      process.stdout.write("\r\n", 2);
+    }
   }
 }
 
