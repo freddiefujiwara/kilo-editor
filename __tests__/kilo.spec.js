bcd449011282363cf186d6dfce142b854ed9aeba
Fumikazu Fujiwara <github@ze.gs>
2020-10-31 18:44:07 +0900

    add jest --collect-coverage

--- __tests__/kilo.spec.js
+++ __tests__/kilo.spec.js
@@ -39,40 +39,132 @@ describe('Kilo', () => {
   });
   it(' editorReadKey(str,key) : can switch regarding each input', () => {
     const k = new Kilo(['LICENSE']);
-    k.E.screenrows = 40;
-    k.E.screencols = 40;
+    k.E.screenrows = 10;
+    k.E.screencols = 10;
     k.editorOpen();
     k.editorRefreshScreen = () => {};
     expect(k.editorReadKey).toBeInstanceOf(Function);
     // vertical cursor move
     k.editorReadKey('',{name:'down'});
+    k.editorScroll();
     expect(k.E.cy).toEqual(1);
+
     k.editorReadKey('',{name:'pagedown'});
-    expect(k.E.cy).toEqual(21);
+    k.editorScroll();
+    expect(k.E.cy).toEqual(19);
+    expect(k.E.rowoff).toEqual(10);
+
     k.editorReadKey('',{name:'down'});
-    expect(k.E.cy).toEqual(21);
-    k.editorReadKey('',{name:'up'});
+    k.editorScroll();
     expect(k.E.cy).toEqual(20);
+    expect(k.E.rowoff).toEqual(11);
+
+    k.editorReadKey('',{name:'pagedown'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(21);
+    expect(k.E.rowoff).toEqual(12);
+
+    k.editorReadKey('',{name:'up'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(20);
+    expect(k.E.rowoff).toEqual(12);
+
     k.editorReadKey('',{name:'pageup'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(2);
+    expect(k.E.rowoff).toEqual(2);
+
+    k.editorReadKey('',{name:'pageup'});
+    k.editorScroll();
     expect(k.E.cy).toEqual(0);
+    expect(k.E.rowoff).toEqual(0);
+
     // horizontal cursor move
     k.editorReadKey('',{name:'right'});
+    k.editorScroll();
     expect(k.E.cx).toEqual(1);
+    expect(k.E.coloff).toEqual(0);
+
     k.editorReadKey('',{name:'end'});
+    k.editorScroll();
     expect(k.E.cx).toEqual(11);
+    expect(k.E.coloff).toEqual(2);
+
     k.editorReadKey('',{name:'left'});
+    k.editorScroll();
     expect(k.E.cx).toEqual(10);
+    expect(k.E.coloff).toEqual(2);
+
     k.editorReadKey('',{name:'right'});
+    k.editorScroll();
     expect(k.E.cx).toEqual(11);
-    k.editorReadKey('',{name:'home'});
+    expect(k.E.coloff).toEqual(2);
+
+    k.editorReadKey('',{name:'right'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(1);
     expect(k.E.cx).toEqual(0);
+    expect(k.E.coloff).toEqual(0);
+
+    k.editorReadKey('',{name:'left'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(0);
+    expect(k.E.cx).toEqual(11);
+    expect(k.E.coloff).toEqual(2);
+
+    k.editorReadKey('',{name:'home'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(0);
+    expect(k.E.cx).toEqual(0);
+    expect(k.E.coloff).toEqual(0);
+
+    k.editorReadKey('',{name:'down'});
+    k.editorScroll();
+    k.editorReadKey('',{name:'down'});
+    k.editorScroll();
+    k.editorReadKey('',{name:'end'});
+    k.editorScroll();
+    k.editorReadKey('',{name:'up'});
+    k.editorScroll();
+    expect(k.E.cy).toEqual(1);
+    expect(k.E.cx).toEqual(0);
+    expect(k.E.coloff).toEqual(0);
   });
   it(' editorRefreshScreen() : can refresh the screen', () => {
     const k = new Kilo();
     expect(k.editorRefreshScreen).toBeInstanceOf(Function);
   });
-  it(' editorDrawRows() : can draw lines', () => {
+  it(' editorDrawStatusBar() : can draw lines', () => {
     const k = new Kilo(['LICENSE']);
+    expect(k.editorDrawStatusBar).toBeInstanceOf(Function);
+    expect(k.abuf.length).toBe(0);
+    k.editorDrawStatusBar();
+    expect(k.abuf.length).not.toBe(0);
+  });
+  it(' editorDrawMessageBar() : can draw lines', () => {
+    const k = new Kilo(['LICENSE']);
+    expect(k.editorDrawMessageBar).toBeInstanceOf(Function);
+    expect(k.abuf.length).toBe(0);
+    k.editorDrawMessageBar();
+    expect(k.abuf.length).not.toBe(0);
+  });
+  it(' editorDrawRows() : can draw lines', () => {
+    let k = new Kilo(['LICENSE']);
     expect(k.editorDrawRows).toBeInstanceOf(Function);
+    expect(k.abuf.length).toBe(0);
+    k.editorDrawRows();
+    expect(k.abuf.length).not.toBe(0);
+
+    k = new Kilo(['LICENSE']);
+    k.E.screenrows = 10;
+    k.E.screencols = 10;
+    expect(k.abuf.length).toBe(0);
+    k.editorDrawRows();
+    expect(k.abuf.length).not.toBe(0);
+
+    k = new Kilo([]);
+    expect(k.abuf.length).toBe(0);
+    k.editorDrawRows();
+    expect(k.abuf.length).not.toBe(0);
   });
 });
