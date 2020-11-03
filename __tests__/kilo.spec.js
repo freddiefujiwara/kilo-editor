fb541c3f76bd1512df0f6b45dc210741f3253793
Fumikazu Fujiwara <github@ze.gs>
2020-11-03 20:02:46 +0900

    test for editorSave complated

--- __tests__/kilo.spec.js
+++ __tests__/kilo.spec.js
@@ -5,38 +5,37 @@ describe("Kilo", () => {
     const variables = {};
 
     it(" constructor() : can create new instance", () => {
-        const k = new Kilo();
-
-        expect(k).not.toBeNull();
-        expect(k).toBeInstanceOf(Kilo);
-        expect(k.E.cx).toEqual(0);
-        expect(k.E.cy).toEqual(0);
-        expect(k.E.erow).toEqual([]);
-        expect(k.E.rowoff).toEqual(0);
-        expect(k.E.erow.length).toEqual(0);
-        expect(k.E.screenrows).not.toEqual(0);
-        expect(k.E.screencols).not.toEqual(0);
+        expect(variables.k).not.toBeNull();
+        expect(variables.k).toBeInstanceOf(Kilo);
+        expect(variables.k.E.cx).toEqual(0);
+        expect(variables.k.E.cy).toEqual(0);
+        expect(variables.k.E.erow).toEqual([]);
+        expect(variables.k.E.rowoff).toEqual(0);
+        expect(variables.k.E.erow.length).toEqual(0);
+        expect(variables.k.E.screenrows).not.toEqual(0);
+        expect(variables.k.E.screencols).not.toEqual(0);
     });
     it(" editorOpen() : can read all strings from file", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorOpen).toBeInstanceOf(Function);
-        k.editorOpen();
-        expect(k.E.erow.length).toEqual(21);
+        expect(variables.k.editorOpen).toBeInstanceOf(Function);
+        variables.k.editorOpen();
+        expect(variables.k.E.erow.length).toEqual(21);
 
         // no such file or directory
         expect(() => {
-            const qi = new Kilo("__tests__/testData.csv");
+            const qi = new Kilo(["__tests__/testData.csv"]);
 
             qi.editorOpen();
         }).toThrow(/no such file or directory/u);
     });
     it(" editorSave() : can save properly", () => {
-        const k = new Kilo(["LICENSE"]);
+        expect(variables.k.editorSave).toBeInstanceOf(Function);
+        variables.k.editorOpen();
+        variables.k.editorSave();
+        expect(variables.k.E.statusmsg).toEqual("1083 bytes written to disk");
 
-        expect(k.editorSave).toBeInstanceOf(Function);
-        k.editorOpen();
-        k.editorSave();
+        variables.k = new Kilo(["__test__/testData.csv"]);
+        variables.k.editorSave();
+        expect(variables.k.E.statusmsg).toEqual("Error:ENOENT: no such file or directory, open '__test__/testData.csv'");
     });
     it(" enableRawMode() : can set tty from normal to raw mode", () => {
         expect(Kilo.enableRawMode).toBeInstanceOf(Function);
@@ -47,150 +46,138 @@ describe("Kilo", () => {
         Kilo.disableRawMode();
     });
     it(" editorScroll() : can culculate scroll", () => {
-        const k = new Kilo();
-
-        expect(k.editorScroll).toBeInstanceOf(Function);
+        expect(variables.k.editorScroll).toBeInstanceOf(Function);
     });
     it(" editorReadKey(str,key) : can switch regarding each input", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        k.E.screenrows = 10;
-        k.E.screencols = 10;
-        k.editorOpen();
-        k.editorRefreshScreen = () => {};
-        expect(k.editorReadKey).toBeInstanceOf(Function);
+        variables.k.E.screenrows = 10;
+        variables.k.E.screencols = 10;
+        variables.k.editorOpen();
+        variables.k.editorRefreshScreen = () => {};
+        expect(variables.k.editorReadKey).toBeInstanceOf(Function);
 
         // vertical cursor move
-        k.editorReadKey("", { name: "down" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(1);
+        variables.k.editorReadKey("", { name: "down" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(1);
 
-        k.editorReadKey("", { name: "pagedown" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(19);
-        expect(k.E.rowoff).toEqual(10);
+        variables.k.editorReadKey("", { name: "pagedown" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(19);
+        expect(variables.k.E.rowoff).toEqual(10);
 
-        k.editorReadKey("", { name: "down" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(20);
-        expect(k.E.rowoff).toEqual(11);
+        variables.k.editorReadKey("", { name: "down" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(20);
+        expect(variables.k.E.rowoff).toEqual(11);
 
-        k.editorReadKey("", { name: "pagedown" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(21);
-        expect(k.E.rowoff).toEqual(12);
+        variables.k.editorReadKey("", { name: "pagedown" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(21);
+        expect(variables.k.E.rowoff).toEqual(12);
 
-        k.editorReadKey("", { name: "up" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(20);
-        expect(k.E.rowoff).toEqual(12);
+        variables.k.editorReadKey("", { name: "up" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(20);
+        expect(variables.k.E.rowoff).toEqual(12);
 
-        k.editorReadKey("", { name: "pageup" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(2);
-        expect(k.E.rowoff).toEqual(2);
+        variables.k.editorReadKey("", { name: "pageup" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(2);
+        expect(variables.k.E.rowoff).toEqual(2);
 
-        k.editorReadKey("", { name: "pageup" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(0);
-        expect(k.E.rowoff).toEqual(0);
+        variables.k.editorReadKey("", { name: "pageup" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(0);
+        expect(variables.k.E.rowoff).toEqual(0);
 
         // horizontal cursor move
-        k.editorReadKey("", { name: "right" });
-        k.editorScroll();
-        expect(k.E.cx).toEqual(1);
-        expect(k.E.coloff).toEqual(0);
+        variables.k.editorReadKey("", { name: "right" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cx).toEqual(1);
+        expect(variables.k.E.coloff).toEqual(0);
 
-        k.editorReadKey("", { name: "end" });
-        k.editorScroll();
-        expect(k.E.cx).toEqual(11);
-        expect(k.E.coloff).toEqual(2);
+        variables.k.editorReadKey("", { name: "end" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cx).toEqual(11);
+        expect(variables.k.E.coloff).toEqual(2);
 
-        k.editorReadKey("", { name: "left" });
-        k.editorScroll();
-        expect(k.E.cx).toEqual(10);
-        expect(k.E.coloff).toEqual(2);
+        variables.k.editorReadKey("", { name: "left" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cx).toEqual(10);
+        expect(variables.k.E.coloff).toEqual(2);
 
-        k.editorReadKey("", { name: "right" });
-        k.editorScroll();
-        expect(k.E.cx).toEqual(11);
-        expect(k.E.coloff).toEqual(2);
+        variables.k.editorReadKey("", { name: "right" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cx).toEqual(11);
+        expect(variables.k.E.coloff).toEqual(2);
 
-        k.editorReadKey("", { name: "right" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(1);
-        expect(k.E.cx).toEqual(0);
-        expect(k.E.coloff).toEqual(0);
+        variables.k.editorReadKey("", { name: "right" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(1);
+        expect(variables.k.E.cx).toEqual(0);
+        expect(variables.k.E.coloff).toEqual(0);
 
-        k.editorReadKey("", { name: "left" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(0);
-        expect(k.E.cx).toEqual(11);
-        expect(k.E.coloff).toEqual(2);
+        variables.k.editorReadKey("", { name: "left" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(0);
+        expect(variables.k.E.cx).toEqual(11);
+        expect(variables.k.E.coloff).toEqual(2);
 
-        k.editorReadKey("", { name: "home" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(0);
-        expect(k.E.cx).toEqual(0);
-        expect(k.E.coloff).toEqual(0);
+        variables.k.editorReadKey("", { name: "home" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(0);
+        expect(variables.k.E.cx).toEqual(0);
+        expect(variables.k.E.coloff).toEqual(0);
 
-        k.editorReadKey("", { name: "down" });
-        k.editorScroll();
-        k.editorReadKey("", { name: "down" });
-        k.editorScroll();
-        k.editorReadKey("", { name: "end" });
-        k.editorScroll();
-        k.editorReadKey("", { name: "up" });
-        k.editorScroll();
-        expect(k.E.cy).toEqual(1);
-        expect(k.E.cx).toEqual(0);
-        expect(k.E.coloff).toEqual(0);
+        variables.k.editorReadKey("", { name: "down" });
+        variables.k.editorScroll();
+        variables.k.editorReadKey("", { name: "down" });
+        variables.k.editorScroll();
+        variables.k.editorReadKey("", { name: "end" });
+        variables.k.editorScroll();
+        variables.k.editorReadKey("", { name: "up" });
+        variables.k.editorScroll();
+        expect(variables.k.E.cy).toEqual(1);
+        expect(variables.k.E.cx).toEqual(0);
+        expect(variables.k.E.coloff).toEqual(0);
     });
     it(" editorRefreshScreen() : can refresh the screen", () => {
-        const k = new Kilo();
-
-        expect(k.editorRefreshScreen).toBeInstanceOf(Function);
-        k.editorRefreshScreen();
+        expect(variables.k.editorRefreshScreen).toBeInstanceOf(Function);
+        variables.k.editorRefreshScreen();
     });
     it(" editorDrawStatusBar() : can draw lines", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorDrawStatusBar).toBeInstanceOf(Function);
-        expect(k.abuf.length).toBe(0);
-        k.editorDrawStatusBar();
-        expect(k.abuf.length).not.toBe(0);
+        expect(variables.k.editorDrawStatusBar).toBeInstanceOf(Function);
+        expect(variables.k.abuf.length).toBe(0);
+        variables.k.editorDrawStatusBar();
+        expect(variables.k.abuf.length).not.toBe(0);
     });
     it(" editorDrawMessageBar() : can draw lines", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorDrawMessageBar).toBeInstanceOf(Function);
-        expect(k.abuf.length).toBe(0);
-        k.editorDrawMessageBar();
-        expect(k.abuf.length).not.toBe(0);
+        expect(variables.k.editorDrawMessageBar).toBeInstanceOf(Function);
+        expect(variables.k.abuf.length).toBe(0);
+        variables.k.editorDrawMessageBar();
+        expect(variables.k.abuf.length).not.toBe(0);
     });
     it(" editorDrawRows() : can draw lines", () => {
-        let k = new Kilo(["LICENSE"]);
+        variables.k.E.screenrows = 100;
+        variables.k.E.screencols = 100;
+        expect(variables.k.editorDrawRows).toBeInstanceOf(Function);
+        expect(variables.k.abuf.length).toBe(0);
+        variables.k.editorDrawRows();
+        expect(variables.k.abuf.length).not.toBe(0);
 
-        k.E.screenrows = 100;
-        k.E.screencols = 100;
-        expect(k.editorDrawRows).toBeInstanceOf(Function);
-        expect(k.abuf.length).toBe(0);
-        k.editorDrawRows();
-        expect(k.abuf.length).not.toBe(0);
+        variables.k = new Kilo(["LICENSE"]);
+        variables.k.E.screenrows = 10;
+        variables.k.E.screencols = 10;
+        expect(variables.k.abuf.length).toBe(0);
+        variables.k.editorDrawRows();
+        expect(variables.k.abuf.length).not.toBe(0);
 
-        k = new Kilo(["LICENSE"]);
-        k.E.screenrows = 10;
-        k.E.screencols = 10;
-        expect(k.abuf.length).toBe(0);
-        k.editorDrawRows();
-        expect(k.abuf.length).not.toBe(0);
-
-        k = new Kilo([]);
-        k.E.screenrows = 100;
-        k.E.screencols = 100;
-        expect(k.abuf.length).toBe(0);
-        k.editorDrawRows();
-        expect(k.abuf.length).not.toBe(0);
+        variables.k = new Kilo([]);
+        variables.k.E.screenrows = 100;
+        variables.k.E.screencols = 100;
+        expect(variables.k.abuf.length).toBe(0);
+        variables.k.editorDrawRows();
+        expect(variables.k.abuf.length).not.toBe(0);
     });
     it(" editorRowCxToRx() : can convert cx -> rx", () => {
         expect(Kilo.editorRowCxToRx).toBeInstanceOf(Function);
@@ -201,59 +188,49 @@ describe("Kilo", () => {
         expect(Kilo.editorUpdateSyntax("test")).toEqual("test");
     });
     it(" editorMoveCursor() : can calculate proper cursor position", () => {
-        const k = new Kilo(["LICENSE"]);
+        variables.k.editorMoveCursor("up");
 
-        k.editorMoveCursor("up");
-
-        expect(k.editorMoveCursor).toBeInstanceOf(Function);
+        expect(variables.k.editorMoveCursor).toBeInstanceOf(Function);
     });
     it(" die() : can exit with proper status code", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.die).toBeInstanceOf(Function);
-        k.die();
+        expect(variables.k.die).toBeInstanceOf(Function);
+        variables.k.die();
 
     });
     it(" editorDelChar() : can delete char", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorDelChar).toBeInstanceOf(Function);
-        k.editorDelChar();
+        expect(variables.k.editorDelChar).toBeInstanceOf(Function);
+        variables.k.editorDelChar();
 
     });
     it(" editorInsertRow(insert) : can insert row", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorInsertRow).toBeInstanceOf(Function);
-        k.editorInsertRow();
+        expect(variables.k.editorInsertRow).toBeInstanceOf(Function);
+        variables.k.editorInsertRow();
 
     });
     it(" editorInsertChar(c) : can insert c", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorInsertChar).toBeInstanceOf(Function);
-        k.editorInsertChar("");
+        expect(variables.k.editorInsertChar).toBeInstanceOf(Function);
+        variables.k.editorInsertChar("");
 
     });
     it(" editorRowDelChar(at) : can delete char at 'at'", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorRowDelChar).toBeInstanceOf(Function);
-        k.editorRowDelChar(0);
+        expect(variables.k.editorRowDelChar).toBeInstanceOf(Function);
+        variables.k.editorRowDelChar(0);
 
     });
     it(" editorRowInsertChar(at,c) : can insert char at 'at'", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.editorRowInsertChar).toBeInstanceOf(Function);
-        k.editorRowInsertChar(0, "");
+        expect(variables.k.editorRowInsertChar).toBeInstanceOf(Function);
+        variables.k.editorRowInsertChar(0, "");
 
     });
     it(" main : can run properly", () => {
-        const k = new Kilo(["LICENSE"]);
-
-        expect(k.main).toBeInstanceOf(Function);
-        k.main();
+        expect(variables.k.main).toBeInstanceOf(Function);
+        variables.k.main();
+    });
+    beforeEach(() => {
+        variables.k = new Kilo(["LICENSE"]);
+    });
+    afterEach(() => {
+        delete variables.k;
     });
     beforeAll(() => {
         variables.error = console.error;
